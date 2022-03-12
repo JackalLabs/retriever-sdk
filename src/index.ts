03db87a8761ebc87cff95e765b8ae9e5caf98229
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2022-03-11 23:23:13 -0500

    updated testnet address

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import { Bech32, fromHex, toHex } from '@cosmjs/encoding'
 import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
 
-export function resolveName (name: string, prefix: string, contractAddress: string = "juno1qr76sfjnr40xulzlymm2ehx5wxwv8pyqnlah5smqtnfrq446guvs42xc6l", rpc: string = "https://rpc.uni.junomint.com:443/"): Promise<string> {
+export function resolveName (name: string, prefix: string = "", contractAddress: string = "juno1kl5cgeuzyyckmnrxtdy8p0dkravc93u7c9806wt3es67qt6qnndshf4l5p", rpc: string = "https://rpc.uni.junomint.com:443/"): Promise<string> {
     return new Promise((resolve, reject) => {
         SigningCosmWasmClient.connect(rpc).then((client) => {
 
+            if ( name.endsWith(".rns") ) {
+                name = name.substring(0, name.length - 4);
+            } 
+
+            let chunks = name.split('.');
+
+            if( prefix.length < 1 ) {
+                prefix = chunks[0];
+
+                if(chunks.length <= 1) {
+                    reject(new Error("If not using the {prefix}.{name} model then you must supply a prefix to the function call."));
+                } 
+            }
+
+            
 
             const msg = { resolve_attributes: { name: name } }
 
